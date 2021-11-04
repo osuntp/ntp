050eba98ac7276ddc2f532cc5df420dce7dd1146
gulanr <gula.8@osu.edu>
2021-11-04 17:46:20 -0400

    PyFoam parameter variation

--- App/OFI.py
+++ App/OFI.py
@@ -3,11 +3,26 @@
 class OFI:
     def __init__(self, file_location):
         self.file_location = file_location
+        self.solver = "simpleFoam"
+        self.case = "case"
 
-    def create_case(self, bc):
+    def writeBC(self):
         pass
 
-    def run_case(self):
+    def startCase(self):
         pass
 
-    
+    def getProgress(self):
+        pass
+
+    def parseResults(self):
+        pass
+
+    def stopCase(self):
+        pass
+
+    def exportResults(self):
+        pass
+
+    def getMassFlow(self):
+        pass
