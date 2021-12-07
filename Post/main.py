664f1114d4297b33e3089d77f4105088c2b7217c
gulanr <gula.8@osu.edu>
2021-12-06 23:11:48 -0500

    Trying some plotting stuff

--- Post/main.py
+++ Post/main.py
@@ -1,6 +1,10 @@
 import logging
 import os
 import datetime
+import pandas as pd
+import seaborn as sns
+import tkinter as tk
+from tkinter import filedialog
 
 def init_logging():
     BASE_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -16,10 +20,26 @@ def init_logging():
     logging.info('Logging started')
     logging.info(f'Log file saved to {LOG_FILE}')
 
+def open_data_file():
+    root = tk.Tk()
+    root.withdraw()
+    logging.info('Prompting user to select data file')
+    return filedialog.askopenfilename()
+
+def load_data_file(f):
+    df = pd.read_table(f,index_col=0)
+    logging.info('Converted data file to data frame')
+    num_rows = len(df)
+    logging.info(f'Dataframe has {num_rows} rows')
+    return df
 
 def main():
     init_logging()
-    
+    f = open_data_file()
+    logging.info(f'Opened {f}')
+
+    df = load_data_file(f)
+    df.head()
 
 if __name__ == "__main__":
     main()
